c757770b13db34363c79493534c5a71cf71dd0f7
rhodanus2179 <32211157+rhodanus2179@users.noreply.github.com>
2026-08-05 00:48:16 +0900

    Restore Yomu Pace service worker

---
+++ yomu-pace/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'yomu-pace-v0.1.0-static-1';
+const ASSETS = [
+  './', './index.html', './styles.css', './manifest.webmanifest',
+  './js/main.js', './js/app.js', './js/ui.js', './js/reader.js', './js/chunking.js', './js/markdown.js', './js/storage.js',
+  './vendor/budoux/budoux.js', './icons/icon.svg'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin || event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    if (response.ok && response.type === 'basic') {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+    }
+    return response;
+  }).catch(() => event.request.mode === 'navigate' ? caches.match('./index.html') : Response.error())));
+});
